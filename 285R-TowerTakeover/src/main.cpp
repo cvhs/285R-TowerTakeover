7c7be495445305cd4edc7dddea43a43e9d7ec66b
Rahul Menon <menonrahul02@gmail.com>
2019-10-14 18:39:02 -0500

    rename/reorganize stuff in main.cpp

--- 285R-TowerTakeover/src/main.cpp
+++ 285R-TowerTakeover/src/main.cpp
@@ -1,6 +1,24 @@
 #include "devices.hpp"
 
 
+ChassisScales scales{ {4.125_in, 10_in}, imev5GreenTPR };
+std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder().withMotors({ -1, -3 }, { 2, 4 }).withGearset(okapi::AbstractMotor::gearset::green).withDimensions(scales).withOdometry(okapi::StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps).build();
+std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
+
+okapi::ControllerButton intakeButton = okapi::ControllerDigital::R2;
+okapi::ControllerButton trayButton = okapi::ControllerDigital::L2;
+okapi::ControllerButton upButton = okapi::ControllerDigital::R1;
+okapi::ControllerButton downButton = okapi::ControllerDigital::L1;
+
+okapi::MotorGroup rollers = MotorGroup({ -12, 14 });
+okapi::Motor tilt = Motor(6);
+okapi::Motor arm = Motor(13);
+
+okapi::Controller controller;
+
+bool toggle = false;
+
+
 void initialize() {}
 
 void disabled() {}
@@ -8,26 +26,6 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {}
-
-//* @param  iscales {straight scale, turn scale} or
-//* {straight scale, turn scale, length to middle wheel in meters, middle scale}
-
-ChassisScales scales{{4.125_in, 10_in}, imev5GreenTPR};
-std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder().withMotors({-1, -3}, {2, 4}).withGearset(okapi::AbstractMotor::gearset::green).withDimensions(scales).withOdometry(okapi::StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps).build();
-std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
-
-okapi::ControllerButton Intake = okapi::ControllerDigital::R2;
-okapi::ControllerButton Tray = okapi::ControllerDigital::L2;
-okapi::ControllerButton Up = okapi::ControllerDigital::R1;
-okapi::ControllerButton Down = okapi::ControllerDigital::L1;
-
-okapi::MotorGroup rollers = MotorGroup({-12, 14});
-okapi::Motor tilt = Motor(6);
-okapi::Motor arm = Motor(13);
-
-okapi::Controller controller;
-
-bool toggle = false;
 
 void opcontrol()
 {
