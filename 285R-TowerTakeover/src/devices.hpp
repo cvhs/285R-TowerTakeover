d56dd0dc910e08d44f2422d18014aee0007d755b
Dan <periodicallydan@gmail.com>
2019-10-15 13:29:44 -0500

    i got to go

--- 285R-TowerTakeover/src/devices.hpp
+++ 285R-TowerTakeover/src/devices.hpp
@@ -1,18 +1,18 @@
 #include "main.h"
-
-extern ChassisScales scales;
-extern std::shared_ptr<okapi::ChassisController> chassis;
-extern std::shared_ptr<okapi::ChassisModel> model;
-
-extern okapi::MotorGroup rollers;
-extern okapi::Motor tilt;
-extern okapi::Motor arm;
-
+//
+// extern ChassisScales scales;
+// extern std::shared_ptr<okapi::OdomChassisController> chassis;
+// extern std::shared_ptr<okapi::ChassisModel> model;
+//
+// extern okapi::MotorGroup rollers;
+// extern okapi::Motor tilt;
+// extern okapi::Motor arm;
+//
 enum trayStates
 {
   off,
   up,
   down
 };
-
-extern trayStates trayState;
+//
+// extern trayStates trayState;
--- 285R-TowerTakeover/src/main.cpp
+++ 285R-TowerTakeover/src/main.cpp
@@ -2,8 +2,15 @@
 
 
 ChassisScales scales{ {4.125_in, 10_in}, imev5GreenTPR };
-std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder().withMotors({ -1, -3 }, { 2, 4 }).withGearset(okapi::AbstractMotor::gearset::green).withDimensions(scales).withOdometry(okapi::StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps).build();
-std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
+// auto chassis = okapi::ChassisControllerBuilder()
+// 																												  .withMotors({ -1, -3 }, { 2, 4 })
+// 																												  // .withGearset(okapi::AbstractMotor::gearset::green)
+// 																												  // .withDimensions(scales)
+// 																													.build();
+// 																												  // .withOdometry(okapi::StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps)
+// 																												  // .buildOdometry();
+// std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
+// std::shared_ptr<okapi::OdomChassisController> chassis = new okapi::OdomChassisController();
 
 okapi::ControllerButton intakeButton = okapi::ControllerDigital::R2;
 okapi::ControllerButton trayButton = okapi::ControllerDigital::L2;
@@ -12,7 +19,7 @@ okapi::ControllerButton downButton = okapi::ControllerDigital::L1;
 
 okapi::MotorGroup rollers = MotorGroup({ -12, 14 });
 okapi::Motor tilt = Motor(6);
-okapi::Motor arm = Motor(13);
+okapi::Motor arm = Motor(-13);
 
 okapi::Controller controller;
 
@@ -30,35 +37,59 @@ void autonomous() {}
 
 void opcontrol()
 {
-	model->tank(controller.getAnalog(okapi::ControllerAnalog::leftY),
-							controller.getAnalog(okapi::ControllerAnalog::rightY));
+	auto chassis = okapi::ChassisControllerBuilder()
+									  .withMotors({ -1, -3 }, { 2, 4 })
+									  .withGearset(okapi::AbstractMotor::gearset::green)
+									  .withDimensions(scales)
+									  .withOdometry(okapi::StateMode::CARTESIAN, 0_mm, 0_deg, 0.0001_mps)
+									  .buildOdometry();
+auto model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
 
-	if(intakeButton.isPressed())
-		rollers.moveVoltage(12000);
-	else
-		rollers.moveVoltage(0);
 
-	if(downButton.isPressed())
-		trayState = up;
-	else if(trayButton.isPressed())
-		trayState = down;
-	else
-		trayState = off;
-
-	switch(trayState)
+	rollers.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	arm.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+	while(1)
 	{
-		case off:
-		tilt.moveVoltage(0);
-		break;
+		model->tank(controller.getAnalog(okapi::ControllerAnalog::rightY),
+								controller.getAnalog(okapi::ControllerAnalog::leftY));
 
-		case up:
-		tilt.moveVoltage(12000);
-		break;
+		if(intakeButton.isPressed())
+			rollers.moveVoltage(12000);
+		else
+			rollers.moveVoltage(0);
 
-		case down:
-		tilt.moveVoltage(-12000);
-		break;
+		if(upButton.isPressed())
+			arm.moveVoltage(12000);
+		else if(downButton.isPressed())
+			arm.moveVoltage(-12000);
+		else
+			arm.moveVoltage(0);
+
+		// if(controller.getDigital(okapi::ControllerDigital::X))
+		if(trayButton.isPressed())
+			toggle = !toggle;
+
+		if(toggle)
+			trayState = up;
+		else
+			trayState = off;
+
+		switch(trayState)
+		{
+			case off:
+			tilt.moveVoltage(0);
+			break;
+
+			case up:
+			// tilt.moveVoltage(12000);
+			tilt.moveAbsolute(1234, 100);
+			break;
+
+			case down:
+			// tilt.moveVoltage(-12000);
+			tilt.moveAbsolute(0, 100);
+			break;
+		}
 	}
-
 
 }
