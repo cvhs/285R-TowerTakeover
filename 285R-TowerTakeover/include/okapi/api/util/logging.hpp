9ffd304b2c0ed8a11b7f180456dccd2f84d36756
Rahul Menon <menonrahul02@gmail.com>
2019-10-27 13:55:00 -0500

    update to okapilib v4-rc6

--- 285R-TowerTakeover/include/okapi/api/util/logging.hpp
+++ 285R-TowerTakeover/include/okapi/api/util/logging.hpp
@@ -18,6 +18,11 @@
 #define LOG_WARN(msg) logger->warn([=]() { return msg; })
 #define LOG_ERROR(msg) logger->error([=]() { return msg; })
 
+#define LOG_DEBUG_S(msg) LOG_DEBUG(std::string(msg))
+#define LOG_INFO_S(msg) LOG_INFO(std::string(msg))
+#define LOG_WARN_S(msg) LOG_WARN(std::string(msg))
+#define LOG_ERROR_S(msg) LOG_ERROR(std::string(msg))
+
 namespace okapi {
 class Logger {
   public:
@@ -141,8 +146,28 @@ class Logger {
   const LogLevel logLevel;
   FILE *logfile;
   CrossplatformMutex logfileMutex;
-  static std::shared_ptr<Logger> defaultLogger;
 
   static bool isSerialStream(std::string_view filename);
 };
+
+extern std::shared_ptr<Logger> defaultLogger;
+
+struct DefaultLoggerInitializer {
+  DefaultLoggerInitializer() {
+    if (count++ == 0) {
+      init();
+    }
+  }
+  ~DefaultLoggerInitializer() {
+    if (--count == 0) {
+      cleanup();
+    }
+  }
+
+  static int count;
+  static void init();
+  static void cleanup();
+};
+
+static DefaultLoggerInitializer defaultLoggerInitializer; // NOLINT(cert-err58-cpp)
 } // namespace okapi
