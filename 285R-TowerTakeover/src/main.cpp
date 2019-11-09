8bfdc2b744bd5f3ffe4fb6880c3228e6b61cac8c
Dan Bizman <43706783+PeriodicallyMe@users.noreply.github.com>
2019-11-08 18:55:05 -0600

    blue aut works - 5 cubes

--- 285R-TowerTakeover/src/main.cpp
+++ 285R-TowerTakeover/src/main.cpp
@@ -52,26 +52,34 @@ void autonomous()
 		rollers.moveVelocity(0);
 		chassis->driveToPoint({3.75_ft, 9.75_ft}, true);
 		chassis->driveToPoint({1.5_ft, 9.0_ft});
+		
 		rollers.moveRelative(-850, 100);
 		tray.setState(TrayController::trayStates::up);
 		while(tray.getState() == TrayController::trayStates::up){}
 		tray.setState(TrayController::trayStates::down);
 		rollers.moveVelocity(-100);
+		pros::delay(200);
 		chassis->moveDistance(-1_ft);
 		rollers.moveVelocity(0);
 	}
 	else
 	{
-		rollers.moveVelocity(80);
+		rollers.moveVelocity(160);
 		chassis->setState({11.5_ft, 9.75_ft, 180_deg});
 		chassis->driveToPoint({7.5_ft, 9.75_ft});
-		pros::delay(300);
+		pros::delay(250);
 		rollers.moveVelocity(0);
-		chassis->driveToPoint({11_ft, 7.2_ft}, false, 40_cm);
-		chassis->waitUntilSettled();
+		chassis->driveToPoint({8.25_ft, 9.75_ft}, true);
+		chassis->driveToPoint({10_ft, 8.15_ft}, false, 0_in);
+
+		rollers.moveRelative(-850, 100);
 		tray.setState(TrayController::trayStates::up);
-		pros::delay(500);
+		while(tray.getState() == TrayController::trayStates::up){}
+		tray.setState(TrayController::trayStates::down);
+		rollers.moveVelocity(-100);
+		pros::delay(200);
 		chassis->moveDistance(-1_ft);
+		rollers.moveVelocity(0);
 	}
 }
 
