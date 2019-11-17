6e9acad534d5896d6141498b0c051ca3a72ff99b
Rahul Menon <menonrahul02@gmail.com>
2019-11-17 15:51:01 -0600

    create stack function to make more readable

--- 285R-TowerTakeover/src/comp/autonFunctions.cpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.cpp
@@ -1,6 +1,21 @@
 #include "../devices/devices.hpp"
 #include "autonFunctions.hpp"
 
+void stack() {
+  // Push bottom cube low enough that it touches ground
+  // TODO: make this more consistent, maybe with sensor?
+  rollers.moveRelative(-850, 100);
+
+  // Stack then back up
+  tray.setState(TrayController::trayStates::up);
+  while(tray.getState() == TrayController::trayStates::up){}
+  tray.setState(TrayController::trayStates::down);
+  rollers.moveVelocity(-100);
+  pros::delay(200);
+  chassis->moveDistance(-1_ft);
+  rollers.moveVelocity(0);
+}
+
 void red5Cubes() {
   // Turn on rollers
   rollers.moveVelocity(160);
@@ -18,19 +33,7 @@ void red5Cubes() {
   chassis->driveToPoint({3.75_ft, 9.75_ft}, true);
   chassis->driveToPoint({1.5_ft, 9.0_ft});
   
-  // Push bottom cube low enough that it touches ground
-  // TODO: make this more consistent, maybe with sensor?
-  rollers.moveRelative(-850, 100);
-
-  // Stack then back up
-  tray.setState(TrayController::trayStates::up);
-  while(tray.getState() == TrayController::trayStates::up){}
-  tray.setState(TrayController::trayStates::down);
-  rollers.moveVelocity(-100);
-  pros::delay(200);
-  chassis->moveDistance(-1_ft);
-  rollers.moveVelocity(0);
-}
+  stack();
 
 void blue5Cubes() {
   // Turn on rollers
@@ -49,16 +52,5 @@ void blue5Cubes() {
   chassis->driveToPoint({8.25_ft, 9.75_ft}, true);
   chassis->driveToPoint({10_ft, 8.15_ft}, false, 0_in);
 
-  // Push bottom cube low enough that it touches ground
-  // TODO: make this more consistent, maybe with sensor?
-  rollers.moveRelative(-850, 100);
-
-  // Stack then back up
-  tray.setState(TrayController::trayStates::up);
-  while(tray.getState() == TrayController::trayStates::up){}
-  tray.setState(TrayController::trayStates::down);
-  rollers.moveVelocity(-100);
-  pros::delay(200);
-  chassis->moveDistance(-1_ft);
-  rollers.moveVelocity(0);
+  stack();
 }
--- 285R-TowerTakeover/src/comp/autonFunctions.hpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.hpp
@@ -1,2 +1,3 @@
+void stack();
 extern void red5Cubes();
 extern void blue5Cubes();
