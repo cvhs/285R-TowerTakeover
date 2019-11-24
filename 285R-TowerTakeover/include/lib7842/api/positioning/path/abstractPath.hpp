0c0cbac9c037e63265ac02bc166b6e3e1845b88c
Rahul Menon <menonrahul02@gmail.com>
2019-11-24 11:52:35 -0600

    add lib7842

---
+++ 285R-TowerTakeover/include/lib7842/api/positioning/path/abstractPath.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace lib7842 {
+
+class SimplePath;
+
+/**
+ * A generic path representation that can be interpolated into a SimplePath.
+ */
+class AbstractPath {
+public:
+  AbstractPath() = default;
+  AbstractPath(const AbstractPath& ipath) = default;
+  AbstractPath(AbstractPath&& ipath) = default;
+  virtual ~AbstractPath() = default;
+
+  /**
+   * Interpolate the path
+   *
+   * @param  isteps how many points to interpolate per segment, from start (inclusive) to end
+   *                (exclusive) of segment.
+   * @return generated path
+   */
+  virtual SimplePath generate(const int isteps = 1) const = 0;
+};
+
+} // namespace lib7842
---
+++ 285R-TowerTakeover/include/lib7842/api/positioning/path/compoundPath.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "abstractPath.hpp"
+#include "simplePath.hpp"
+
+namespace lib7842 {
+
+/**
+ * A path that represents a combination of segments that can be interpolated into a SimplePath.
+ */
+class CompoundPath : public AbstractPath {
+public:
+  CompoundPath() = default;
+
+  /**
+   * Add a path segment.
+   *
+   * @param  isegment The segment.
+   */
+  CompoundPath& add(const std::shared_ptr<AbstractPath>& isegment);
+
+  /**
+   * Interpolate the path
+   *
+   * @param  isteps how many points to interpolate per segment, from start (inclusive) to end
+   *                (exclusive) of segment.
+   * @return generated path
+   */
+  SimplePath generate(const int isteps = 1) const override;
+
+protected:
+  std::vector<std::shared_ptr<AbstractPath>> segments {};
+}; // namespace lib7842
+
+} // namespace lib7842
