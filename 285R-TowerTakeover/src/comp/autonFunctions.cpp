4d87fe2b5358db48c63e1eed98f3ae9ffa7a5b39
Rahul Menon <menonrahul02@gmail.com>
2020-01-12 12:51:30 -0600

    Prototype blue side small zone 8 cube auton

--- 285R-TowerTakeover/src/comp/autonFunctions.cpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.cpp
@@ -171,6 +171,38 @@ void redSmall8Cube() {
   stack();
 }
 
+void blueSmall8Cube() {
+  // Deploy tray and intake
+  deploy();
+
+  // Start rollers and intake the 3 cubes
+  rollers.moveVelocity(200);
+  profiler->setTarget("dx=4 dy=0");
+  profiler->waitUntilSettled();
+
+  // Stop rollers and s-curve back
+  rollers.moveVelocity(0);
+  profiler->setTarget("dx=4 dy=-2", true);
+  profiler->waitUntilSettled();
+
+  // Start rollers and intake 4 cubes
+  rollers.moveVelocity(200);
+  profiler->setTarget("dx=4 dy=2");
+  profiler->waitUntilSettled();
+
+  // Stop rollers and move back
+  rollers.moveVelocity(0);
+  profiler->setTarget("dx=2.5 dy=0", true);
+  profiler->waitUntilSettled();
+
+  // Turn to goal zone and approach
+  autChassis->setState({0_ft, 0_ft, 0_deg});
+  autChassis->turnToAngle(225_deg);
+  autChassis->moveDistance(2_ft);
+
+  stack();
+}
+
 void oneCube() {
   autChassis->moveDistance(-1.5_ft);
   pros::delay(800);
--- 285R-TowerTakeover/src/comp/autonFunctions.hpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.hpp
@@ -11,4 +11,5 @@ extern int aut;
 extern void redSmall5Cube();
 extern void blueSmall5Cube();
 extern void redSmall8Cube();
+extern void blueSmall8Cube();
 extern void oneCube();
