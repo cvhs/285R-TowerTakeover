a78a121060286bdb7091a98a3b602f75dc2f53af
Rahul Menon <menonrahul02@gmail.com>
2020-01-11 22:14:10 -0600

    Prototype red zone 8 cube auton

--- 285R-TowerTakeover/src/comp/autonFunctions.cpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.cpp
@@ -71,7 +71,73 @@ void redSmall5Cube() {
   profiler->waitUntilSettled();
   profiler->removePath("4 Cubes");
 
-  // Stop rollers and move back;
+  // Stop rollers and move back
+  rollers.moveVelocity(0);
+  profiler->setTarget("Reverse to turn", true);
+  profiler->waitUntilSettled();
+
+  // Turn to goal zone and approach
+  autChassis->setState({0_ft, 0_ft, 0_deg});
+  autChassis->turnToAngle(135_deg);
+  autChassis->moveDistance(2_ft);
+
+  stack();
+}
+
+void redSmall8Cube() {
+  // Asynchronously generate first path
+  pros::Task pathgen1([] (void* inp) {
+    profiler->generatePath(
+      {{0_ft, 0_ft, 0_deg},
+       {4_ft, 0_ft, 0_deg}},
+      "3 Cubes"
+    );
+  }, (void*)"inp", TASK_PRIORITY_DEFAULT - 1, 
+  TASK_STACK_DEPTH_DEFAULT, "pathgen1");
+
+  // Deploy tray and intake while path is generating
+  deploy();
+
+  // Check if path is done generating, if not then wait until it is then kill the task
+  std::vector<std::string> paths;
+  do {
+    paths = profiler->getPaths();
+  } while(std::find(paths.begin(), paths.end(), "3 Cubes") != paths.end());
+  pathgen1.remove();
+
+  // Start rollers and intake the 3 cubes, generate the next path
+  rollers.moveVelocity(200);
+  profiler->setTarget("3 Cubes");
+  profiler->generatePath(
+    {{  0_ft, 0_ft, 0_deg},
+     {4_ft, -2_ft, 0_deg}},
+     "Reverse s-curve"
+  );
+  profiler->waitUntilSettled();
+  profiler->removePath("4 Cubes");
+
+  // Stop rollers and s-curve back, generate next path
+  rollers.moveVelocity(0);
+  profiler->setTarget("Reverse to turn", true);
+  profiler->generatePath(
+    {{0_ft, 0_ft, 0_deg},
+     {4_ft, 0_ft, 0_deg}},
+     "4 Cubes"
+  );
+  profiler->waitUntilSettled();
+
+  // Start rollers and intake 4 cubes, generate next path
+  rollers.moveVelocity(200);
+  profiler->setTarget("4 Cubes");
+  profiler->generatePath(
+    {{  0_ft, 0_ft, 0_deg},
+     {2.5_ft, 0_ft, 0_deg}},
+     "Reverse to turn"
+  );
+  profiler->waitUntilSettled();
+  profiler->removePath("4 Cubes");
+
+  // Stop rollers and move back
   rollers.moveVelocity(0);
   profiler->setTarget("Reverse to turn", true);
   profiler->waitUntilSettled();
--- 285R-TowerTakeover/src/comp/autonFunctions.hpp
+++ 285R-TowerTakeover/src/comp/autonFunctions.hpp
@@ -7,4 +7,5 @@ extern void selectAuton();
 extern int aut;
 
 extern void redSmall5Cube();
+extern void redSmall8Cube();
 extern void oneCube();
