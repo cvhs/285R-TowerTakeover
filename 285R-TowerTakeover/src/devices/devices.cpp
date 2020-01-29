77a3d365118458f5395be402c7a2185ce24d2d00
Rahul Menon <menonrahul02@gmail.com>
2020-01-29 17:58:48 -0600

    Move ports to const variables for increased readability and maintenance

--- 285R-TowerTakeover/src/devices/devices.cpp
+++ 285R-TowerTakeover/src/devices/devices.cpp
@@ -7,41 +7,52 @@ okapi::ControllerButton liftUpButton = okapi::ControllerDigital::L1;
 okapi::ControllerButton liftDownButton = okapi::ControllerDigital::L2;
 okapi::ControllerButton	driverDan = okapi::ControllerDigital::B;
 
-okapi::MotorGroup rollers = MotorGroup({ -5, 8 });
+const int rightRollerPort = -5;
+const int leftRollerPort = 8;
+const int liftPort = 4;
+const int trayPort = 11;
+const int blDrivePort = -10;
+const int flDrivePort = -17;
+const int brDrivePort = 15;
+const int frDrivePort = 13;
+
+const okapi::ChassisScales scales
+{
+	{4.125_in, 10.2_in},
+	imev5GreenTPR
+};
+
+okapi::MotorGroup rollers({rightRollerPort, leftRollerPort});
 
 okapi::Motor lift(
-	4, false, 
+	liftPort, false, 
 	okapi::AbstractMotor::gearset::red, 
 	okapi::AbstractMotor::encoderUnits::degrees
 );
 
 std::shared_ptr<okapi::Motor> tray = std::make_shared<okapi::Motor>(
-	11, false, 
+	trayPort, false, 
 	okapi::AbstractMotor::gearset::red, 
 	okapi::AbstractMotor::encoderUnits::degrees
 );
 
 TrayController trayController = TrayController(tray);
 
-okapi::ChassisScales scales
-{
-	{4.125_in, 10.2_in},
-	imev5GreenTPR
-};
 okapi::Controller controller;
 
 std::shared_ptr<okapi::ChassisController> chassis = okapi::ChassisControllerBuilder()
-										.withMotors({ -10, -17 }, { 15, 13 })
+										.withMotors({blDrivePort, flDrivePort}, {brDrivePort, frDrivePort})
 										.withDimensions(okapi::AbstractMotor::gearset::green, scales)
 										.build();
 
 // TODO: tune max velocity
 std::shared_ptr<okapi::OdomChassisController> autChassis = okapi::ChassisControllerBuilder()
-										.withMotors({ -1, -3 }, { 2, 4 })
+										.withMotors({blDrivePort, flDrivePort}, {brDrivePort, frDrivePort})
 										.withDimensions(okapi::AbstractMotor::gearset::green, scales)
 										.withMaxVelocity(75)
 										.withOdometry(okapi::StateMode::FRAME_TRANSFORMATION, 0_mm, 0_deg)
 										.buildOdometry();
+
 std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(chassis->getModel());
 
 std::shared_ptr<okapi::AsyncMotionProfileController> profiler = okapi::AsyncMotionProfileControllerBuilder()
