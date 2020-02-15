77e8bafa262600c760e50cbe4167c0e0ae1f24d6
Rahul Menon <menonrahul02@gmail.com>
2020-02-15 13:14:26 -0600

    Add comments to big zone red auton

--- 285R-TowerTakeover/src/comp/auton/bigZone.cpp
+++ 285R-TowerTakeover/src/comp/auton/bigZone.cpp
@@ -4,25 +4,34 @@
 #include "devices/devices.hpp"
 
 void redBig3Cube() {
+  // Deploy the tray
   deploy();
+
+  // Start rollers and move forward to grab cube in front
   rollers.moveVelocity(200);
   autChassis->moveDistance(1.9_ft);
+
+  // Turn to face closest mid tower and collect cubes, deploy antitips along the
+  // way
   autChassis->turnAngle(-100_deg);
   autChassis->setMaxVelocity(120);
   autChassis->moveDistanceAsync(2.8_ft);
   deployAntitips();
   autChassis->waitUntilSettled();
-  autChassis->setMaxVelocity(80);
   pros::delay(200);
-  autChassis->setMaxVelocity(120);
+
+  // Go back and stop rollers along the way
   autChassis->moveDistanceAsync(-2.8_ft);
   pros::delay(500);
   rollers.moveVelocity(0);
   autChassis->waitUntilSettled();
+
+  // Turn to face goal zone and approach it
   autChassis->setMaxVelocity(80);
   autChassis->turnAngle(-130_deg);
   autChassis->moveDistance(0.5_ft);
 
+  // Spit two cubes into zone, raise lift and spit one on top
   rollers.moveRelative(-2500, 150);
   pros::delay(1000);
   lift.moveRelative(300, 80);
