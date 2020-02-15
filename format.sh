9d4b3e4104f426fa876dfdcb89ba36149f998614
Rahul Menon <menonrahul02@gmail.com>
2020-02-15 12:27:09 -0600

    Fix issues (probably)

--- format.sh
+++ format.sh
@@ -1,10 +1,10 @@
-#!C:/Windows/system32/cmd.exe
+#!/bin/sh
 
-for i in $(find ./285R-TowerTakeover/src/ -iname "*.cpp"); do clang-format -i -style=google $i || true
+for i in $(find ./285R-TowerTakeover/src/ -iname "*.cpp"); do clang-format -i -style=google "$i" || true
 done
 
-for i in $(find ./285R-TowerTakeover/include/devices/ -iname "*.hpp"); do clang-format -i -style=google $i || true
+for i in $(find ./285R-TowerTakeover/include/devices/ -iname "*.hpp"); do clang-format -i -style=google "$i" || true
 done
 
-for i in $(find ./285R-TowerTakeover/include/comp/ -iname "*.hpp"); do clang-format -i -style=google $i || true
+for i in $(find ./285R-TowerTakeover/include/comp/ -iname "*.hpp"); do clang-format -i -style=google "$i" || true
 done
